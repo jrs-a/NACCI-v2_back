97dcec89347ea2aacadb45902bb0617744128b49
jrs-a <jamaicaalberto@yahoo.com>
2023-05-05 02:03:11 +0800

    v0.22 delete api folder rename index.js to server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const activityRoutes = require('../routes/activities')
+const activityRoutes = require('./routes/activities')
 
 // express app
 const app = express()
