4faede5360df8d0002bc2995eb039f9cb5e16c06
jrs-a <jamaicaalberto@yahoo.com>
2023-05-04 20:42:18 +0800

    v0.1 first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
--app.use(ex[ress.urlencoded({ extended: false})]);
+// -app.use(ex[ress.urlencoded({ extended: false})]);
 app.use(
     cors ({
         origin: ['http://localhost:3000', 
