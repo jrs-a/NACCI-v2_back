e7b15764a990993efacc87e77efa8997a15bd515
jrs-a <jamaicaalberto@yahoo.com>
2023-05-06 10:45:58 +0800

    v0.3 added req headers

--- server.js
+++ server.js
@@ -9,38 +9,10 @@ const reservationRoutes = require('./routes/reservations')
 // express app
 const app = express()
 
-// connect to db
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => {
-//     // listen for requests
-//         app.listen(process.env.PORT, () => {
-//             console.log('connected to db & listening on port', process.env.PORT)
-//         })
-//     })
-//     .catch((error) => {
-//         console.log(error)
-//     })
-
-mongoose.set('strictQuery', false);
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log('connected to mongodb', conn.connection.host);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
-
-connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log('listening on port', process.env.PORT);
-    })
-})
-
 // middleware
 app.use(express.json())
 app.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", "*");
     console.log(req.path, req.method)
     next()
 })
@@ -57,3 +29,21 @@ app.use('/api/activities', activityRoutes)
 app.use('/api/reservations', reservationRoutes)
 app.use('/api/user', userRoutes)
 
+
+// mong conn
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log('connected to mongodb', conn.connection.host);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('listening on port', process.env.PORT);
+    })
+})
