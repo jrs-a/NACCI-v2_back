630338c2004d4aa3bbb6a15c5615aef64a1a0ddb
jrs-a <jamaicaalberto@yahoo.com>
2023-05-09 00:05:34 +0800

    v0.46 allow headers

--- server.js
+++ server.js
@@ -13,16 +13,17 @@ const app = express()
 app.use(express.json())
 app.use((req, res, next) => {
     res.append("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log(req.path, req.method)
     next()
 })
 
 // avoid CORS and any POST or OPTIONS error
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- })
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//  })
 
  // routes REQUIRED!
 app.use('/api/activities', activityRoutes)
