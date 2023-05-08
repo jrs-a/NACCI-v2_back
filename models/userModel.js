c92487b4d49ebf6a1ca8d6e5d0fd8ba644b073b1
jrs-a <jamaicaalberto@yahoo.com>
2023-05-08 21:26:42 +0800

    v0.4 add name field on user model

--- models/userModel.js
+++ models/userModel.js
@@ -10,6 +10,10 @@ const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
@@ -22,10 +26,10 @@ const userSchema = new Schema({
 })
 
 // static signup method
-userSchema.statics.signup = async function(email, password) {
+userSchema.statics.signup = async function(name, email, password) {
 
   // validation
-  if (!email || !password) {
+  if (!name || !email || !password) {
     throw Error('All fields must be filled')
   }
   if (!validator.isEmail(email)) {
@@ -44,7 +48,7 @@ userSchema.statics.signup = async function(email, password) {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
-  const user = await this.create({ email, password: hash })
+  const user = await this.create({ name, email, password: hash })
 
   return user
 }
